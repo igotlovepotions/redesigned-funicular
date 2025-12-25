d01730faabe5c564e1448393e15e9cd69a2b7611
Igotlovepotions <69840347+igotlovepotions@users.noreply.github.com>
2025-12-25 16:07:55 -0500

    hum

--- calcs.py
+++ calcs.py
@@ -1,5 +1,6 @@
 import cables from show_cables.py
-
+import math as mt 
+import cmath
 #calcular as distancias d
 
 
@@ -7,3 +8,22 @@ dab
 dac
 dapr1
 dapr2
+dba 
+dbc
+dbpr1
+dbpr2
+dca
+dcb 
+dcpr1
+dcpr2
+
+## usa-se o metodo do condutor pelo solo 
+
+## DE = 658.37 * math.sqrt ( res / freq) freq = 60 hz , res = resistividade do solo, 100 ohms
+
+##primeira matriz 
+
+## indutancia
+
+
+
